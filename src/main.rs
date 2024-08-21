00b813a2251d0c8d5330f01ab899b14a7aa20c6e
nccvector <fzn1020@gmail.com>
2024-08-21 09:48:02 +0900

    add raylib for visual rays debugging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![allow(non_snake_case)]
 
+use raylib::color::Color as RaylibColor;
 use eframe::{egui, App, NativeOptions};
 use eframe::egui::{Image, Rect, Ui};
 use eframe::egui::CentralPanel;
-
-const VERTICAL_FOV: f32 = 45.0;
-const IMAGE_WIDTH: u32 = 640;
-const IMAGE_HEIGHT: u32 = 480;
+use eframe::egui::ImageData::{Color as EguiColor, Color};
+use raylib::ffi::{DrawCube, GenMeshCylinder};
+use raylib::prelude::*;
 
 mod vec_ops;
 mod camera;
@@ -37,11 +37,63 @@ impl App for Renderer {
 }
 
 fn main() {
-    let options = NativeOptions::default();
+    let mut renderer = Renderer::new();
 
+    // let (mut rl, thread) = raylib::init().size(800, 600).title("Hello").build();
+    //
+    // let mut camera = Camera3D::perspective(
+    //     Vector3::new(4.0, 2.0, 4.0),
+    //     Vector3::new(0.0, 1.8, 0.0),
+    //     Vector3::new(0.0, 1.0, 0.0),
+    //     60.0,
+    // );
+    // camera.position = Vector3::new(3.5, 0.65, -3.0);
+    // camera.target = Vector3::new(0.0, 0.0, 0.0);
+    //
+    // let meshCyl = unsafe { WeakMesh::from_raw(GenMeshCylinder(0.025, 1.0, 10)) };
+    // let modelCyl = rl.load_model_from_mesh(&thread, meshCyl).unwrap();
+    //
+    // rl.set_target_fps(60);
+    //
+    // while !rl.window_should_close() {
+    //     let mut d = rl.begin_drawing(&thread);
+    //     d.clear_background(RaylibColor::WHITE);
+    //
+    //     d.update_camera(&mut camera, CameraMode::CAMERA_FIRST_PERSON);
+    //
+    //     {
+    //         let mut d2 = d.begin_mode3D(camera);
+    //
+    //         d2.draw_model_ex(&modelCyl, Vector3::zero(), Vector3::forward(), -90.0, Vector3::one(), RaylibColor::RED);
+    //         d2.draw_model_ex(&modelCyl, Vector3::zero(), Vector3::forward(), 0.0, Vector3::one(), RaylibColor::GREEN);
+    //         d2.draw_model_ex(&modelCyl, Vector3::zero(), Vector3::right(), 90.0, Vector3::one(), RaylibColor::BLUE);
+    //
+    //         let rays = renderer.camera.getRays();
+    //         for mut ray in rays {
+    //             ray.3 /= ray.5.abs();
+    //             ray.4 /= ray.5.abs();
+    //             ray.5 /= ray.5.abs();
+    //             d2.draw_line_3D(
+    //                 Vector3::new(ray.0, ray.1, ray.2),
+    //                 Vector3::new(ray.0 + ray.3, ray.1 + ray.4, ray.2 + ray.5),
+    //                 RaylibColor::new(100, 100, 100, 100),
+    //             );
+    //
+    //             d2.draw_circle_3D(
+    //                 Vector3::new(ray.0 + ray.3, ray.1 + ray.4, ray.2 + ray.5),
+    //                 0.005,
+    //                 Vector3::forward(),
+    //                 0.0,
+    //                 RaylibColor::RED,
+    //             );
+    //         }
+    //     }
+    // }
+
+    let options = NativeOptions::default();
     eframe::run_native(
         "",
         options,
-        Box::new(|cc| Ok(Box::new(Renderer::new()))),
+        Box::new(|cc| Ok(Box::new(renderer))),
     ).unwrap();
 }
